6e0e7e71180d09398cba7351a66b428905cb2112
Pranav <pranavmasekar4@gmail.com>
2022-10-30 18:40:14 +0530

    Project setu p

---
+++ lib/asset_helper.dart
@@ -0,0 +1,17 @@
+class AssetHelper {
+  static const String imagePath = "assets/images/";
+  static const String awardsPath = "assets/images/awards/";
+
+  static const String googlePng = "${imagePath}google.png";
+  static const String loginEmotePng = "${imagePath}loginEmote.png";
+  static const String logoPng = "${imagePath}logo.png";
+
+  static const String awesomeAnsPng = "${awardsPath}awesomeanswer.png";
+  static const String goldPng = "${awardsPath}gold.png";
+  static const String helpfulPng = "${awardsPath}helpful.png";
+  static const String platinumPng = "${awardsPath}platinum.png";
+  static const String plusonePng = "${awardsPath}plusone.png";
+  static const String rocketPng = "${awardsPath}rocket.png";
+  static const String thankYouPng = "${awardsPath}thankyou.png";
+  static const String tilPng = "${awardsPath}til.png";
+}
